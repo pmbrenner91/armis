9b283820fbfa9f48bf028ae50687775b0e8d483f
pmbrenner91 <pmbrenner91@users.noreply.github.com>
2018-11-30 18:47:14 -0700

    Create armis_assessor.sql

---
+++ armis_assessor.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE armis_assessor;
+CREATE TABLE controls (
+	control_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+	control_description varchar(255) DEFAULT NULL,
+	information_source varchar(255) DEFAULT NULL,
+	control_benefit int(11) DEFAULT NULL,
+	control_cost int(11) DEFAULT NULL,
+	control_impact_score int(11) DEFAULT NULL,
+	UNIQUE (control_ID),
+	PRIMARY KEY (control_id)
+);
+
+LOCK TABLES controls WRITE;
+
+INSERT INTO controls ()
+VALUES
+	()
+	();
+
+CREATE TABLE systems (
+	system_id int(11) unsigned NOT NULL AUTO_INCREMENT,
+	system_description varchar(255) DEFAULT NULL,
+	UNIQUE (system_ID),
+	PRIMARY KEY (system_id)
+);
